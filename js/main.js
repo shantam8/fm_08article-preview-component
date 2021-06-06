b174c8f8ae3778e927deb837e133311d0fe28901
shantam8 <dscharmen@aol.com>
2021-06-06 23:57:09 +0200

    mobile done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
+function openShareMenu() {
+  let user = document.querySelector(".user");
+  if (user.style.display == "none") {
+    console.log("1");
+    user.style.display = "grid";
+    document.querySelector(".share").style.display = "none";
+    document.querySelector("footer").style.backgroundColor = "white";
+  } else {
+    console.log("2");
+    user.style.display = "none";
+    document.querySelector(".share").style.display = "grid";
+    document.querySelector("footer").style.backgroundColor =
+      "hsl(217, 19%, 35%)";
+  }
+}
 
-function init(){
-    console.log("test");
+function init() {
+  let svgBtns = document.querySelectorAll(".svgBtn");
+    svgBtns.forEach(el =>{
+        el.addEventListener("click", openShareMenu);
+    })
+
 }
 
 window.addEventListener("load", init);
