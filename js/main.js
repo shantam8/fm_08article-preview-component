59acafe83e88ba4b483fff93bf1a4c2c443cdd2b
shantam8 <dscharmen@aol.com>
2021-06-07 11:02:36 +0200

    desktop complete, minor bugfixes

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
+let veryDarkGrayishBlue = "hsl(217, 19%, 35%)";
+let desaturatedDarkBlue = "hsl(214, 17%, 51%)";
+let grayishBlue = "hsl(212, 23%, 69%)";
+let lightGrayishBlue = "hsl(210, 46%, 95%)";
+
 function openShareMenu() {
+  let svgBtn = document.querySelector(".svgBtn");
+  let svgShareIcon = document.querySelector(".svgSharePath");
   let user = document.querySelector(".user");
   let share = document.querySelector(".share");
   let footer = document.querySelector("footer");
-  if (user.style.display == "none") {
-    user.style.display = "grid";
-    share.style.display = "none";
-    footer.style.backgroundColor = "white";
+
+  if (window.innerWidth >= 1200) {
+    //desktop
+    if (svgBtn.classList.contains("shareMenuOpen")) {
+      svgBtn.classList.remove("shareMenuOpen");
+      share.style.display = "none";
+    } else {
+      svgBtn.classList.add("shareMenuOpen");
+      share.style.display = "grid";
+    }
   } else {
-    user.style.display = "none";
-    share.style.display = "grid";
-    footer.style.backgroundColor = "hsl(217, 19%, 35%)";
+    //mobile
+    if (svgBtn.classList.contains("shareMenuOpen")) {
+      svgBtn.classList.remove("shareMenuOpen");
+      svgBtn.style.backgroundColor = lightGrayishBlue;
+      svgShareIcon.style.fill = grayishBlue;
+      user.style.display = "grid";
+      share.style.display = "none";
+      footer.style.backgroundColor = "white";
+    } else {
+      svgBtn.classList.add("shareMenuOpen");
+      svgBtn.style.backgroundColor = desaturatedDarkBlue;
+      svgShareIcon.style.fill = "white";
+      user.style.display = "none";
+      share.style.display = "grid";
+      footer.style.backgroundColor = veryDarkGrayishBlue;
+    }
   }
 }
 
 function init() {
-  let svgBtns = document.querySelectorAll(".svgBtn");
-  svgBtns.forEach((el) => {
-    el.addEventListener("click", openShareMenu);
-  });
+  document.querySelector(".svgBtn").addEventListener("click", openShareMenu);
 }
 
 window.addEventListener("load", init);
